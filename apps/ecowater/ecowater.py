b13dcb574c3f163a1e31a2d7c5990e503bb6816b
Antonios Karagiannis <antokarag@gmail.com>
2021-05-09 23:13:43 -0600

    test timer

--- apps/ecowater/ecowater.py
+++ apps/ecowater/ecowater.py
@@ -2,10 +2,8 @@ import mqttapi as mqtt
 
 class EcoWater(mqtt.Mqtt):
     def initialize(self):
-        # Create a time object for 7pm
-        time = datetime.time(19, 00, 0)
-        # Schedule a daily callback that will call run_daily() at 7pm every night
-        self.run_daily(self.run_fn, time)
+        self.run_every(self.run_fn, datetime.datetime.now() + timedelta(seconds=3), 1 * 60)
+	
 
     def run_fn(self, kwargs):
         self.mqtt_publish('ecowater/test', 5)
